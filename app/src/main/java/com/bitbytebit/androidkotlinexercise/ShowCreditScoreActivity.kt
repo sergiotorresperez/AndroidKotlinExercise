6c6025b74d03920102561520fa9cd8dcf77af1ff
Sergio Torres <sergiotorresperez@gmail.com>
2018-03-10 12:23:53 +0000

    Project structure created

---
+++ app/src/main/java/com/bitbytebit/androidkotlinexercise/ShowCreditScoreActivity.kt
@@ -0,0 +1,12 @@
+package com.bitbytebit.androidkotlinexercise
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowCreditScoreActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_credit_score)
+    }
+}
