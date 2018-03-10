10087b4cb75e91411c30ba818bb371fe5937aae0
Sergio Torres <sergiotorresperez@gmail.com>
2018-03-10 13:53:10 +0000

    Implemente retrofit servide to GET credit scores

--- app/src/main/java/com/bitbytebit/androidkotlinexercise/viewcreditscore/data/CreditScoreService.kt
+++ app/src/main/java/com/bitbytebit/androidkotlinexercise/viewcreditscore/data/CreditScoreService.kt
@@ -1,5 +1,14 @@
 package com.bitbytebit.androidkotlinexercise.viewcreditscore.data
 
+import retrofit2.Call
+import retrofit2.http.GET
+
 /**
- * Created by garrapeta on 10/03/18.
+ * Declaration of credit scores HTTP API for Retrofit
  */
+interface CreditScoreService {
+
+    // TODO: the environment (prod) should not be part of the path and should be configured somewhere else
+    @GET("/prod/mockcredit/values")
+    fun getCreditScore() : Call<CreditScoreGetResponse>
+}
