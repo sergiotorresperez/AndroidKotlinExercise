66bf1843bcb4265209462c75a221db536e7e60fc
Sergio Torres <sergiotorresperez@gmail.com>
2018-03-10 12:36:08 +0000

    Imported retrofit and rx java in gradle

--- app/src/main/java/com/bitbytebit/androidkotlinexercise/viewcreditscore/ShowCreditScoreActivity.kt
+++ app/src/main/java/com/bitbytebit/androidkotlinexercise/viewcreditscore/ShowCreditScoreActivity.kt
@@ -1,7 +1,8 @@
-package com.bitbytebit.androidkotlinexercise
+package com.bitbytebit.androidkotlinexercise.viewcreditscore
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.bitbytebit.androidkotlinexercise.R
 
 class ShowCreditScoreActivity : AppCompatActivity() {
 
---
+++ app/src/main/java/com/bitbytebit/androidkotlinexercise/viewcreditscore/data/CreditScoreService.kt
@@ -0,0 +1,5 @@
+package com.bitbytebit.androidkotlinexercise.viewcreditscore.data
+
+/**
+ * Created by garrapeta on 10/03/18.
+ */
