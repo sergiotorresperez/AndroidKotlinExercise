38b526f288ac63c159017bbfbe559144ccb1da46
Sergio Torres <sergiotorresperez@gmail.com>
2018-03-11 01:24:03 +0000

    Created CreditScoreView

--- app/src/main/java/com/bitbytebit/androidkotlinexercise/showcreditscore/presentation/ShowCreditScoreActivity.kt
+++ app/src/main/java/com/bitbytebit/androidkotlinexercise/showcreditscore/presentation/ShowCreditScoreActivity.kt
@@ -1,22 +1,34 @@
 package com.bitbytebit.androidkotlinexercise.showcreditscore.presentation
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.animation.DecelerateInterpolator
 import com.bitbytebit.androidkotlinexercise.R
 import com.bitbytebit.androidkotlinexercise.from
 import com.bitbytebit.androidkotlinexercise.showcreditscore.di.ShowCreditScoreActivityModule
 import com.bitbytebit.androidkotlinexercise.showcreditscore.domain.CreditScore
+import kotlinx.android.synthetic.main.activity_show_credit_score.*
 import javax.inject.Inject
+
+
 
 
 class ShowCreditScoreActivity : AppCompatActivity(), ShowCreditScorePresenter.View {
 
+    companion object {
+        private const val ANIMATION_DURATION_MS = 2000L
+    }
+
     @Inject
     lateinit var presenter : ShowCreditScorePresenter
 
+    private var isRecreated = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        isRecreated = savedInstanceState != null
         setContentView(R.layout.activity_show_credit_score)
 
         application.from(this)
@@ -36,7 +48,21 @@ class ShowCreditScoreActivity : AppCompatActivity(), ShowCreditScorePresenter.Vi
 
     override fun showCreditScore(creditScore: CreditScore) {
         Log.i("ShowCreditScore", "gotten $creditScore")
+
+        creditScoreView.min = creditScore.min
+        creditScoreView.max = creditScore.max
+
+        if (!isRecreated) {
+            val animation = ObjectAnimator.ofInt(creditScoreView, "score", 0, creditScore.score)
+            animation.duration = ANIMATION_DURATION_MS
+            animation.interpolator = DecelerateInterpolator()
+            animation.start()
+        } else {
+            creditScoreView.score = creditScore.score
+        }
+
     }
+
 
     override fun showGetCreditScoreError(error: Throwable) {
         Log.e("ShowCreditScore", "error", error)
---
+++ app/src/main/java/com/bitbytebit/androidkotlinexercise/showcreditscore/presentation/widget/CreditScoreView.kt
@@ -0,0 +1,38 @@
+package com.bitbytebit.androidkotlinexercise.showcreditscore.presentation.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import com.bitbytebit.androidkotlinexercise.R
+import kotlinx.android.synthetic.main.view_credit_score.view.*
+
+class CreditScoreView : FrameLayout {
+
+    var min : Int = 0
+
+    var max : Int
+        get() = min + creditScoreProgressBar.max
+        set(value) {
+            creditScoreProgressBar.max = max - min
+            creditScoreMaxTextView.text = String.format(context.getString(R.string.credit_score_view_max), value)
+        }
+
+    var score : Int
+    get() = creditScoreProgressBar.progress
+    set(value) {
+        creditScoreProgressBar.progress = value - min
+        creditScoreTextView.text = (value - min).toString()
+    }
+
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init(context)
+    }
+
+    private fun init(context: Context) {
+        LayoutInflater.from(context).inflate(R.layout.view_credit_score, this, true)
+    }
+}
